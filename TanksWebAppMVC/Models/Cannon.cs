5b29857f9d855a52c093ce9bdf2fc9b30f9c0d03
Åsa Jonsson <asaj71@gmail.com>
2021-11-17 20:36:15 +0100

    Add project files.

---
+++ TanksWebAppMVC/Models/Cannon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TanksWebAppMVC.Models
+{
+    public class Cannon
+    {
+        public string Designation { get; set; }
+        public double Caliber { get; set; }
+        public double ShellVelocity { get; set; }
+        public int MaxRange { get; set; }
+        public int ClipSize { get; set; }
+        public double IntraClipReload { get; set; }
+        public double ReloadSpeed { get; set; }
+
+    }
+}
+
---
+++ TanksWebAppMVC/Models/Tank.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TanksWebAppMVC.Models
+{
+    public class Tank
+    {
+        public string Type { get; set; }
+        public string Designation { get; set; }
+        public Country Country { get; set; }
+        public string Designer { get; set; }
+        public int Designed { get; set; }
+        public string Manufacturer { get; set; }
+        public double Mass { get; set; }
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public int Crew { get; set; }
+        public Cannon MainArmament { get; set; }
+        public int AmmoCapacity { get; set; }
+    }
+}
